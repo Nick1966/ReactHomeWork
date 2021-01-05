5f314e12929684f457a27ccd5c3546b0b9fbfa86
Nick1966 <nvakoliouk@gmail.com>
2021-01-04 17:17:36 -0800

    Initialize project using Create React App

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,12 +3,18 @@ import {Button, Container, FormControl, Navbar, NavbarBrand, NavLink, Nav, Form}
 import logo from './logo192.png';
 import NavbarToggle from "react-bootstrap/NavbarToggle";
 import NavbarCollapse from "react-bootstrap/NavbarCollapse";
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+//import Switch from "react-bootstrap/Switch";
+import Home from "../Pages/Home";
+import About from "../Pages/About";
+import Contacts from "../Pages/Contacts";
+import Blog from "../Pages/Blog";
 
 export default class Header extends Component {
     render() {
         return (
             <>
-                <Navbar fixed="top" collapseOnSelect expand="md" bg="dark" variant="dark">
+                <Navbar collapseOnSelect expand="md" bg="dark" variant="dark">
                     <Container>
                         <NavbarBrand href="/">
                             <img
@@ -17,7 +23,7 @@ export default class Header extends Component {
                                 width="30"
                                 className="d-inline-block align-top"
                                 alt="Logo"
-                            />
+                            /> React Site
                         </NavbarBrand>
                         <NavbarToggle aria-controls="responsive-navbar-nav"/>
                         <NavbarCollapse id="responsive-navbar-nav">
@@ -38,6 +44,15 @@ export default class Header extends Component {
                         </NavbarCollapse>
                     </Container>
                 </Navbar>
+
+                <Router>
+                    <Switch>
+                        <Route exact path="/" component={Home}/>
+                        <Route exact path="/about" component={About}/>
+                        <Route exact path="/contacts" component={Contacts}/>
+                        <Route exact path="/blog" component={Blog}/>
+                    </Switch>
+                </Router>
             </>
         );
     }
